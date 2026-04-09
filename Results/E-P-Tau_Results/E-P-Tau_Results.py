d433dc9bad9c374be4ec569968fff227f1d3e29a
JasonL-gif <40403366@live.napier.ac.uk>
2026-04-09 19:05:10 +0100

    Update E-P-Tau_Results.py

--- Results/E-P-Tau_Results/E-P-Tau_Results.py
+++ Results/E-P-Tau_Results/E-P-Tau_Results.py
@@ -1,23 +1,23 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data1 = pd.read_csv("Results/csv_bin/csv_1/script_1_data_energy_power.csv")
+data1 = pd.read_csv("Results/csv_bin/csv_1/script_1_data_energy_power.csv")  # Extraction of energy and power data from .csv files
 data2 = pd.read_csv("Results/csv_bin/csv_2/script_2_data_energy_power.csv")
 data3 = pd.read_csv("Results/csv_bin/csv_3/script_3_data_energy_power.csv")
 
-data4 = pd.read_csv("Results/csv_bin/csv_1/script_1_data_torque.csv")
+data4 = pd.read_csv("Results/csv_bin/csv_1/script_1_data_torque.csv")  # Extraction of torque data from .csv files
 data5 = pd.read_csv("Results/csv_bin/csv_2/script_2_data_torque.csv")
 data6 = pd.read_csv("Results/csv_bin/csv_3/script_3_data_torque.csv")
 
 ## ENERGY ##
-plt.subplot(1, 3, 1)
+plt.subplot(1, 3, 1)  # Produces page with several plots
 plt.title("E-t; Trajectory 1")         # Title of plot.
 plt.grid() 
 plt.axhline(0, 0, 1, color = (0, 0, 0, 1))      # Produces a horizontal line at q = 0.
-plt.plot(data1["Time"], data1["Energy"])
-plt.xlabel("Time (s)")
+plt.plot(data1["Time"], data1["Energy"])  # Plots time for the x-axis, and energy for the y-axis
+plt.xlabel("Time (s)")  # Plot labels
 plt.ylabel("Energy Consumption/cycle (J)")
-plt.ylim(0, 80)
+plt.ylim(0, 80)  # Establishes a maximum point of the y-axis for better viewing of results
 #---------#
 plt.subplot(1, 3, 2)
 plt.title("E-t; Trajectory 2")         # Title of plot.
