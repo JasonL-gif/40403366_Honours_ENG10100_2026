201c5a595ea111bc6370ee70882358730d6e321c
JasonL-gif <40403366@live.napier.ac.uk>
2026-04-15 00:54:35 +0100

    Update script_4.py

--- Trajectory_4/script_4.py
+++ Trajectory_4/script_4.py
@@ -60,7 +60,6 @@ with mujoco.viewer.launch_passive(m, d) as viewer:              # With the MuJoC
                 d.ctrl[6] = gripper1_linear(d.time)
                 mujoco.mj_step(m, d)
                 time_elapsed.append(d.time)
-                print(d.time)
         # Trajectory Path
                 pos = d.xpos[11]
                 x_pos = pos[0]
